11d4d44f2652d1d58195aa0cb3c69ce3783f65d5
Ramiro <ramiro.soler44@gmail.com>
2022-11-07 00:52:24 -0300

    MenuScripts

--- My project/Assets/Scenes/MenuControl.cs
+++ My project/Assets/Scenes/MenuControl.cs
@@ -4,32 +4,66 @@ using UnityEngine.SceneManagement;
 public class MenuControl : MonoBehaviour
 {
 
-    public Slider volumenSlider;
+
+
+    public Slider slider;
+    public Toggle toggle;
+
+    public float sliderValue;
+    
 
 
     void Start()
     {
         Values();
+        ToggleOn();
     }
 
 
     void Update()
     {
-
+        if (toggle.isOn)
+        {
+            PlayerPrefs.SetInt("IsOn", 1);
+        }
+        else
+        {
+            PlayerPrefs.SetInt("IsOn", 0);
+        }
     }
+
+
+    void Values() 
+    {
+        slider.value = PlayerPrefs.GetFloat("save", sliderValue);
+    }
+
+    public void SetVolume(float value)
+    {
+        sliderValue = value;
+        PlayerPrefs.SetFloat("save", sliderValue);
+    }
+
+    public void ToggleOn()
+    {
+        bool IsOn = false;
+        if(PlayerPrefs.GetInt("IsOn") == 1)
+        {
+            IsOn = true;
+        }
+
+        else
+        {
+            IsOn = false;
+        }
+
+        toggle.isOn = IsOn;
+    }
+
 
     public void ChanceScene()
     {
         SceneManager.LoadScene("Principal");
     }
 
-    void Values() 
-    {
-        volumenSlider.value = PlayerPrefs.GetFloat("volumerSlider");
-    }
-
-    public void SetVolume()
-    {
-        PlayerPrefs.GetFloat("volumeSlider", volumenSlider.value);
-    }
 }
